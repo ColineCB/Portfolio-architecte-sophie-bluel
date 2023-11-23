42118704de59162f61e07059ef2863cfaf141e9c
Coline Chalude <coline.chalude@gmail.com>
2023-11-23 11:45:14 +0100

    modification des fichiers index.html et fichier.js pour ajouter une div contenant les boutons filtres sous le titre Mes Projets

--- FrontEnd/fichier.js
+++ FrontEnd/fichier.js
@@ -1,10 +1,11 @@
 const gallery = document.querySelector(".gallery");
 const apiUrl = "http://localhost:5678/api/works";
 
+// affichage dynamique des projets
 fetch (apiUrl)
     .then (response => {
         if (!response.ok) {
-            throw new Error ('La requête a échoué');
+            throw new Error ("La requête a échoué");
         }
         return response.json();
     })
@@ -25,5 +26,42 @@ fetch (apiUrl)
         }
     })
     .catch(error => {
-        console.error ('Erreur de fetch', error);
+        console.error ('Erreur de fetch pour les projets', error);
     });
+
+
+const apiCategories = "http://localhost:5678/api/categories";
+
+// Création d'une div pour placer les boutons des filtres
+let divBtnFiltres = document.createElement("div");
+divBtnFiltres.classList.add("div-Btn-Filtres");
+let titreH2 = document.querySelector(".titre-mes-projets");
+if (titreH2) {
+    titreH2.parentNode.insertBefore(divBtnFiltres, titreH2.nextSibling);
+} else {
+    console.error("Titre h2 non trouvé");
+};
+
+// affichage dynamique des trois boutons dans la divBtnFiltres
+fetch (apiCategories)
+    .then (response => {
+        if (!response.ok) {
+            throw new Error ("La requête a échoué");
+        }
+        return response.json();
+    })
+    .then (categories => {
+        console.log(categories);
+        for (const categorie of categories) {
+            const btn = document.createElement("button");
+            btn.textContent = categorie.name;
+            divBtnFiltres.appendChild(btn);
+        }
+    })
+    .catch(error => {
+        console.error ("Erreur de fetch pour les boutons", error);
+    });
+
+// Création du quatrième bouton "Tous", situé en premier dans la liste des quatre boutons
+let btnTous = document.createElement("button");
+divBtnFiltres.appendChild(btnTous); 
