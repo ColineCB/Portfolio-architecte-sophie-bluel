dc19e8294807e4e63c2904cd9aaa5bec145e6d00
Coline Chalude <coline.chalude@gmail.com>
2023-11-28 11:49:13 +0100

    modification du fetch

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,4 @@
-const PROBLEME = ... 
+const apiUrl = "http://localhost:5678/users/login";
 
 // Création de l'événement click pour déclencher la vérification des champs du formulaire
 document.getElementById("loginButton").addEventListener("click", function() {
@@ -6,7 +6,7 @@ document.getElementById("loginButton").addEventListener("click", function() {
     let password = document.getElementById("password").value;
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
     if (email === "exemple@email.com" && password === "mot de passe") {
-        fetch(PROBLEME, {
+        fetch(apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
