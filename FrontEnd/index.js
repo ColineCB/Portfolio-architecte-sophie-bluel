2db33b307bcd7f05030180e2c3d2c326eee1ab3e
Coline Chalude <coline.chalude@gmail.com>
2023-12-04 15:19:22 +0100

    gestion du login / logout et ajout de la modale de la galerie - pas encore fonctionnelle

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -36,12 +36,13 @@ const apiCategories = "http://localhost:5678/api/categories";
 
 // Création d'une div pour placer les boutons des filtres
 let divBtnFiltres = document.createElement("div");
-divBtnFiltres.classList.add("div-Btn-Filtres");
-let titreH2 = document.querySelector(".titre-mes-projets");
+divBtnFiltres.setAttribute("id", "div_Btn_Filtres");
+let titreH2 = document.querySelector(".mes_projets_mode_edition");
 if (titreH2) {
     titreH2.parentNode.insertBefore(divBtnFiltres, titreH2.nextSibling);
 } else {
-    console.error("Titre h2 non trouvé");
+    console.error("div non trouvée");
+
 };
 
 // affichage dynamique du bouton "Tous" et des trois boutons dans la divBtnFiltres
@@ -96,3 +97,25 @@ fetch (apiCategories)
     .catch(error => {
         console.error ("Erreur de fetch pour les boutons", error);
     });
+
+// gestion de la connexion => présence du token
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    const bandeauEdition = document.getElementById("bandeau_edition");
+    const boutonEdition = document.getElementById("bouton_edition");
+    const divBtnFiltres = document.getElementById("div_Btn_Filtres");
+    const boutonLogin = document.getElementById("bouton_login");
+    const boutonLogout = document.getElementById("bouton_logout");
+
+    if (localStorage.getItem('token') !== null) {
+        bandeauEdition.classList.remove("hidden");
+        boutonEdition.classList.remove("hidden");
+        divBtnFiltres.classList.add("visibility_hidden");
+        boutonLogin.classList.add("hidden");
+        boutonLogout.classList.remove("hidden");
+    }
+    /* gestion de la déconnexion */
+    bouton_logout.addEventListener ("click", () => {
+        localStorage.removeItem("token");
+    });
+});
